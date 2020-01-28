89e53893004a0188453428c901735c654ac6e1e7
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-28 14:14:43 -0800

    created route for components in app.js, build the coomponents

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="container">
-      Hello world!
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
