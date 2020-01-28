f0efca4e24847d4d117bceb9eca347153635fb85
Ralphie Celedon <ralphie94@pacbell.net>
2020-01-28 13:47:16 -0800

    created delete exercise route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -29,6 +29,12 @@ router.route("/:id").get((req, res) => {
     Exercise.findById(req.params.id)
         .then(exercise => res.json(exercise))
         .catch(err => res.status(400).json("Error: " + err));
-})
+});
+
+router.route("/:id").delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Exercise deleted."))
+        .catch(err => res.status(400).json("Error: " + err));
+});
 
 module.exports = router;
